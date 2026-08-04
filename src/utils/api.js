0fe55fe00873a81d0d284dd4c22255c3de9ca767
Nenterro <huzaifasadeem@gmail.com>
2026-08-04 11:17:12 +0500

    Fix API fallback URLs used by data fetching utility

--- src/utils/api.js
+++ src/utils/api.js
@@ -11,9 +11,8 @@
  */
 
 const DEFAULT_URLS = [
-  'http://192.168.18.49:8000',
-  'http://100.97.146.42:8000',
-  'https://huz-solar.duckdns.org'
+  'https://huz-solar.duckdns.org:8888',
+  'http://192.168.18.49:8000'
 ];
 
 let cachedWorkingUrl = null;
